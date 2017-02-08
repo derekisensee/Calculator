efe0f9ba66c1c402260f6474a4d2bcad0abff187
Isensee <17isenseed@kisdtx.com>
2017-02-08 14:36:59 -0600

    Add more buttons, number viewing functionality

--- app/src/main/java/com/calc/isenseed/calculator/MainActivity.java
+++ app/src/main/java/com/calc/isenseed/calculator/MainActivity.java
@@ -1,78 +1,89 @@
 package com.calc.isenseed.calculator;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 import static com.calc.isenseed.calculator.R.id.button0;
-import static com.calc.isenseed.calculator.R.id.button1;
-import static com.calc.isenseed.calculator.R.id.button2;
-import static com.calc.isenseed.calculator.R.id.button3;
-import static com.calc.isenseed.calculator.R.id.button4;
-import static com.calc.isenseed.calculator.R.id.button5;
-import static com.calc.isenseed.calculator.R.id.button6;
-import static com.calc.isenseed.calculator.R.id.button7;
-import static com.calc.isenseed.calculator.R.id.button8;
-import static com.calc.isenseed.calculator.R.id.button9;
 
 public class MainActivity extends AppCompatActivity {
+    ArrayList<String> a;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        a = new ArrayList<>();
     }
 
     public void addString(View view) {
-        //Button b = (Button)findViewById(R.id.button0);
-
         TextView t = (TextView)findViewById(R.id.currentNum);
 
         switch (view.getId()) {
             case button0:
                 Button b = (Button)findViewById(R.id.button0);
-                t.setText(b.getText().toString());
+                a.add(b.getText().toString());
                 break;
             case R.id.button1:
                 Button b1 = (Button)findViewById(R.id.button1);
-                t.setText(b1.getText().toString());
+                a.add(b1.getText().toString());
                 break;
             case R.id.button2:
                 Button b2 = (Button)findViewById(R.id.button2);
-                t.setText(b2.getText().toString());
+                a.add(b2.getText().toString());
                 break;
             case R.id.button3:
                 Button b3 = (Button)findViewById(R.id.button3);
-                t.setText(b3.getText().toString());
+                a.add(b3.getText().toString());
                 break;
             case R.id.button4:
                 Button b4 = (Button)findViewById(R.id.button4);
-                t.setText(b4.getText().toString());
+                a.add(b4.getText().toString());
                 break;
             case R.id.button5:
                 Button b5 = (Button)findViewById(R.id.button5);
-                t.setText(b5.getText().toString());
+                a.add(b5.getText().toString());
                 break;
             case R.id.button6:
                 Button b6 = (Button)findViewById(R.id.button6);
-                t.setText(b6.getText().toString());
+                a.add(b6.getText().toString());
                 break;
             case R.id.button7:
                 Button b7 = (Button)findViewById(R.id.button7);
-                t.setText(b7.getText().toString());
+                a.add(b7.getText().toString());
                 break;
             case R.id.button8:
                 Button b8 = (Button)findViewById(R.id.button8);
-                t.setText(b8.getText().toString());
+                a.add(b8.getText().toString());
                 break;
             case R.id.button9:
                 Button b9 = (Button)findViewById(R.id.button9);
-                t.setText(b9.getText().toString());
+                a.add(b9.getText().toString());
                 break;
-
+            case R.id.add:
+                Button add = (Button)findViewById(R.id.add);
+                a.add(add.getText().toString());
+                break;
+            case R.id.subtract:
+                Button subtract = (Button)findViewById(R.id.subtract);
+                a.add(subtract.getText().toString());
+                break;
+            case R.id.multiply:
+                Button multiply = (Button)findViewById(R.id.multiply);
+                a.add(multiply.getText().toString());
+                break;
+            case R.id.divide:
+                Button divide = (Button)findViewById(R.id.divide);
+                a.add(divide.getText().toString());
+                break;
+            case R.id.clear:
+                a = new ArrayList<>();
+                break;
         }
+        t.setText(a.toString());
     }
 }
